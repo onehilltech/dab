206268d2be4425c99c4309a1a77716cc07c9ba8f
James H. Hill <hilljh82@gmail.com>
2017-03-26 12:31:23 -0400

    Added computed to test case

--- tests/lib/pass/resolveTest.js
+++ tests/lib/pass/resolveTest.js
@@ -5,12 +5,18 @@ const expect  = require ('chai').expect
   , datamodel = require ('../../../lib')
   ;
 
+
 describe ('lib.pass.resolve', function () {
+  function computeEmail (data, opts, callback) {
+    var email = this.first_name.toLowerCase() + '.' + this.last_name.toLowerCase() + '@tester.com';
+    return callback (null, email)
+  }
+
   it ('should generate ids for all entries in the data model', function (done) {
     var data = {
       users: [
-        {_id: new ObjectId (), first_name: 'James', last_name: 'Hill'},
-        {_id: new ObjectId (), first_name: 'Lewis', last_name: 'Williams'}
+        {_id: new ObjectId (), first_name: 'James', last_name: 'Hill', email: datamodel.computed (computeEmail)},
+        {_id: new ObjectId (), first_name: 'Lewis', last_name: 'Williams', email: datamodel.computed (computeEmail)}
       ],
 
       friendships: [
@@ -25,6 +31,9 @@ describe ('lib.pass.resolve', function () {
       function (data, callback) {
         expect (data.friendships[0].src).to.eql (data.users[0]._id);
         expect (data.friendships[0].dst).to.eql (data.users[1]._id);
+
+        expect (data.users[0].email).to.equal ('james.hill@tester.com');
+        expect (data.users[1].email).to.equal ('lewis.williams@tester.com');
 
         return callback (null);
       }
