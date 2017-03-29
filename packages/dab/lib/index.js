fe331cd739d01fd71603281a0d1bb25d669cda11
James H. Hill <hilljh82@gmail.com>
2017-03-28 21:49:15 -0400

    Implemented dab.clear

--- packages/dab/lib/index.js
+++ packages/dab/lib/index.js
@@ -6,3 +6,4 @@ const async = require ('async')
 var exports = module.exports = require ('./resolvers');
 exports.build = require ('./build');
 exports.seed = require ('./seed');
+exports.clear = require ('./clear');
