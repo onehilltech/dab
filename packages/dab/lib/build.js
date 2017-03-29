f3d8fbf979c28fbdb37d4e94736385635dee902d
James H. Hill <hilljh82@gmail.com>
2017-03-28 21:37:52 -0400

    Fixed dab.build for complex data models

--- packages/dab/lib/build.js
+++ packages/dab/lib/build.js
@@ -8,17 +8,40 @@ const async     = require ('async')
 function build (data, opts, callback) {
   if (!callback) {
     callback = opts;
-    opts = {};
+    opts = { };
   }
 
-  const phases = [
-    async.constant (data),
-    resolve (opts),
-    generateIds (opts),
-    resolve (opts)
-  ];
+  const maxIterations = opts.maxIterations || 2;
 
-  async.waterfall (phases, callback);
+  var complete = false;
+  var iteration = 0;
+
+  async.doWhilst (
+    function (callback) {
+      async.waterfall ([
+        async.constant (data),
+        generateIds (opts),
+        resolve (opts),
+
+        function (result, unresolved, callback) {
+          if (unresolved === 0)
+            complete = true;
+
+          // Move to the next iteration.
+          ++ iteration;
+
+          if (iteration >= maxIterations)
+            return callback (new Error ('reached max iterations'));
+
+          return callback (null, result);
+        }
+      ], callback);
+    },
+
+    function () {
+      return !complete;
+    },
+    callback);
 }
 
 module.exports = build;
