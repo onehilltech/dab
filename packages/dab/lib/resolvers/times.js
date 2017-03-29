d886782b5addb04f75c64edf8c4c8638d7abf0d2
James H. Hill <hilljh82@gmail.com>
2017-03-29 09:58:08 -0400

    times() would exceed its max call stack for large datasets

--- packages/dab/lib/resolvers/times.js
+++ packages/dab/lib/resolvers/times.js
@@ -1,11 +1,22 @@
 'use strict';
 
-const async = require ('async');
+const async = require ('async')
+  , debug   = require ('debug') ('dab:times')
+  ;
 
 function times (n, func) {
   return function __dabTimes (data, opts, callback) {
     async.times (n, function (i, next) {
-      func.call (data, i, opts, next);
+      if (n % 100 === 0) {
+        debug ('generating item ' + i + ' of ' + n);
+
+        process.nextTick (function () {
+          func.call (data, i, opts, next);
+        });
+      }
+      else {
+        func.call (data, i, opts, next);
+      }
     }, callback);
   }
 }
