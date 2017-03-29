3232ac490f36c1eb9e430f6329950d372c84dad5
James H. Hill <hilljh82@gmail.com>
2017-03-29 10:31:30 -0400

    Build did not set data for next loop

--- packages/dab/lib/build.js
+++ packages/dab/lib/build.js
@@ -3,7 +3,11 @@
 const async     = require ('async')
   , generateIds = require ('./phase/generateIds')
   , resolve     = require ('./phase/resolve')
+  , debug       = require ('debug') ('dab:build')
+  , util        = require ('util')
   ;
+
+const DEFAULT_MAX_ITERATIONS = 10;
 
 function build (data, opts, callback) {
   if (!callback) {
@@ -11,27 +15,40 @@ function build (data, opts, callback) {
     opts = { };
   }
 
-  const maxIterations = opts.maxIterations || 2;
+  const maxIterations = opts.maxIterations || DEFAULT_MAX_ITERATIONS;
 
   var complete = false;
   var iteration = 0;
+  var curr = data;
 
   async.doWhilst (
     function (callback) {
+      debug ('building model (iteration ' + (iteration + 1) + ')');
+
       async.waterfall ([
-        async.constant (data),
+        async.constant (curr),
         generateIds (opts),
         resolve (opts),
 
         function (result, unresolved, callback) {
-          if (unresolved === 0)
+          if (unresolved === 0) {
             complete = true;
+          }
+          else {
+            // Set the current result as the data for the next iteration.
+            debug ('unresolved references: ' + unresolved);
+            debug ('intermediate: ' + util.inspect (result));
 
-          // Move to the next iteration.
+            curr = result;
+          }
+
+          // Move to the next iteration
           ++ iteration;
 
-          if (iteration >= maxIterations)
+          if (iteration >= maxIterations) {
+            debug ('intermediate: ' + util.inspect (result));
             return callback (new Error ('reached max iterations'));
+          }
 
           return callback (null, result);
         }
