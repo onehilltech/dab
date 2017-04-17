0ebfdf63d28d1225c287b6ca3bc8d4effddfc275
James H. Hill <hilljh82@gmail.com>
2017-04-17 16:41:42 -0400

    Implemented dab.omit() and dab.clone() for objects

--- packages/dab/lib/resolvers/index.js
+++ packages/dab/lib/resolvers/index.js
@@ -3,12 +3,14 @@
 var exports = module.exports = {};
 
 exports.get = require ('./get');
+exports.clone = require ('./clone');
 exports.computed = require ('./computed');
 exports.concat = require ('./concat');
 exports.filter = require ('./filter');
-exports.map = require ('./map');
-exports.ref = require ('./ref');
-exports.shuffle = require ('./shuffle');
-exports.sample = require ('./sample');
-exports.times = require ('./times');
 exports.id = require ('./id');
+exports.map = require ('./map');
+exports.omit = require ('./omit');
+exports.ref = require ('./ref');
+exports.sample = require ('./sample');
+exports.shuffle = require ('./shuffle');
+exports.times = require ('./times');
---
+++ packages/dab/lib/resolvers/omit.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const async  = require ('async')
+  , _        = require ('underscore')
+;
+
+function omit (value, keys) {
+  return function __dabClone (callback) {
+    this.resolve (value, function (err, result) {
+      if (err)
+        return callback (err);
+
+      if (result === undefined)
+        return callback (null, undefined);
+
+      return callback (null, _.omit (result, keys));
+    }, callback);
+  }
+}
+
+module.exports = omit;
