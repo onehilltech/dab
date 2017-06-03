f4fa2d8efbf675edbf959b83b1951d3dbcb4e616
James H. Hill <hilljh82@gmail.com>
2017-06-03 02:12:18 -0400

    Switched to using commander for cli

--- packages/dab/examples/firstdab.js
+++ packages/dab/examples/firstdab.js
@@ -8,16 +8,16 @@ module.exports = {
     return callback (null, value);
   }),
 
-  comments: dab.times (10000, function (i, opts, callback) {
-    var user = dab.ref (dab.sample (dab.get ('users')));
-    var comment = {user: user, comment: 'This is comment ' + i};
+  comments: dab.times (1000, function (i, opts, callback) {
+    let user = dab.ref (dab.sample (dab.get ('users')));
+    let comment = {user: user, comment: 'This is comment ' + i};
 
     return callback (null, comment);
   }),
 
   likes: dab.sample (
     dab.map (dab.get ('users'), function (item, opts, callback) {
-      var like = {user: item._id, comment: dab.sample (dab.get ('comments'))};
+      let like = {user: item._id, comment: dab.sample (dab.get ('comments'))};
       return callback (null, like);
     }), dab.randomInt (50, 500))
 };
--- packages/dab/lib/resolvers/concat.js
+++ packages/dab/lib/resolvers/concat.js
@@ -7,12 +7,20 @@ function concat () {
   var arrays = [].slice.call (arguments);
 
   return function __dabConcat (callback) {
-    async.concat (arrays,
-      function (val, callback) {
-        this.resolve (val, callback);
-      }.bind (this),
-      callback);
-  }.bind (this);
+    this.resolve (arrays, function (err, result) {
+      if (err)
+        return callback (err);
+
+      if (result === undefined)
+        return callback (null, undefined);
+
+      async.concat (arrays,
+        function (val, callback) {
+          this.resolve (val, callback);
+        }.bind (this),
+        callback);
+    }.bind (this));
+  };
 }
 
 module.exports = concat;
