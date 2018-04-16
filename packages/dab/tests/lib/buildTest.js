7ea637b92baba1bf365ebba57ff838c9efc5c746
James H. Hill <hilljh82@gmail.com>
2018-04-16 10:08:41 -0400

    Fixed build tests

--- packages/dab/tests/lib/buildTest.js
+++ packages/dab/tests/lib/buildTest.js
@@ -1,13 +1,39 @@
-const expect = require ('chai').expect
-  , mongoose = require ('mongoose')
-  , async    = require ('async')
-  , ObjectId = mongoose.Types.ObjectId
-  , build    = require ('../../lib/build')
-  , dab      = require ('../../lib')
-  ;
+/*
+ * Copyright (c) 2018 One Hill Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 
-describe ('lib.build', function () {
-  it ('should generate on objects (before)', function (done) {
+const {
+  expect
+} = require ('chai');
+
+const {
+  Types: {
+    ObjectId
+  }
+} = require ('mongoose');
+
+const {
+  some
+} = require ('lodash');
+
+const build = require ('../../lib/build');
+const dab   = require ('../../lib');
+
+describe ('lib | build', function () {
+  it ('should build objects on single iteration', function () {
     let data = {
       users: [
         {_id: dab.id (), first_name: 'Jane', last_name: 'Doe'}
@@ -18,21 +44,16 @@ describe ('lib.build', function () {
       ]
     };
 
-    build (data, function (err, result) {
-      if (err)
-        return done (err);
-
+    return build (data).then (result => {
       expect (result).to.have.nested.property ('users[0]._id').that.is.instanceof (ObjectId);
 
       expect (result.comments).to.have.length (1);
       expect (result).to.have.nested.property ('comments[0]._id').that.is.instanceof (ObjectId);
       expect (result).to.have.nested.property ('comments[0].user').that.eql (result.users[0]._id);
-
-      return done (null);
     });
   });
 
-  it ('should generate on objects (after)', function (done) {
+  it ('should resolve unresolved paths', function () {
     let data = {
       comments: [
         {user: dab.ref ('users.0'), comment: 'This is a simple comment'}
@@ -43,63 +64,47 @@ describe ('lib.build', function () {
       ]
     };
 
-    build (data, function (err, result) {
-      if (err)
-        return done (err);
-
+    return build (data).then (result => {
       expect (result).to.have.nested.property ('users[0]._id').that.is.instanceof (ObjectId);
 
       expect (result.comments).to.have.length (1);
       expect (result).to.have.nested.property ('comments[0]._id').that.is.instanceof (ObjectId);
       expect (result).to.have.nested.property ('comments[0].user').that.eql (result.users[0]._id);
-
-      return done (null);
     });
   });
 
-  it ('should build a model with composed resolvers', function (done) {
+  it ('should build model with composed resolvers', function () {
     let data = {
       mapped: dab.map (
-        dab.times (5, function (i, opts, callback) {
-          return callback (null, {username: 'username' + i});
-        }),
-        function (value, opts, callback) {
+        dab.times (5, function (i) { return {username: `username${i}`}; }),
+        function (value) {
           value.password = value.username;
-          return callback (null, value);
+          return value;
         })
     };
 
-    build (data, function (err, result) {
-      if (err)
-        return done (err);
-
+    return build (data).then (result => {
       expect (result.mapped).to.have.length (5);
 
       for (let i = 0; i < 5; ++ i)
-        expect (result).to.have.nested.property ('mapped[' + i + '].username').that.equals ('username' + i);
-
-      return done (null);
+        expect (result).to.have.nested.property (`mapped[${i}].username`).that.equals (`username${i}`);
     });
   });
 
-  it ('should build a model with embedded resolvers', function (done) {
+  it ('should build a model with embedded resolvers', function () {
     let data = {
       users: [
         {first_name: 'Jane', last_name: 'Doe'},
         {first_name: 'John', last_name: 'Doe'}
       ],
 
-      comments: dab.times (5, function (i, opts, callback) {
-        let user = dab.ref (dab.sample (dab.get ('users')));
-        let comment = {user: user, comment: 'This is comment ' + i};
-        return callback (null, comment);
+      comments: dab.times (5, function (i) {
+        let user = dab.ref (dab.sample (this.get ('users')));
+        return {user, comment: `This is comment ${i}`};
       })
     };
 
-    build (data, function (err, result) {
-      if (err)
-        return done (err);
-
+    return build (data).then (result => {
       // check the users
       expect (result).to.have.nested.property ('users[0]._id').that.is.instanceof (ObjectId);
       expect (result).to.have.nested.property ('users[1]._id').that.is.instanceof (ObjectId);
@@ -107,51 +112,39 @@ describe ('lib.build', function () {
       // check the comments
       expect (result.comments).to.have.length (5);
 
-      async.each (result.comments, function (item, callback) {
+      result.comments.forEach (item => {
         expect (item).to.have.deep.property ('user').that.is.instanceof (ObjectId);
 
-        async.some (result.users, function (user, callback) {
-          return callback (null, user._id.equals (item.user));
-        }, callback);
-      }, done);
+        expect (some (result.users, user => user._id.equals (item.user))).to.be.true;
+      });
     });
   });
 
-  it ('should build a model with unresolved values', function (done) {
+  it ('should build a model with unresolved values', function () {
     let data = {
-      users: dab.times (2, function (i, opts, callback) {
-        return callback (null, {name: 'User' + i});
-      }),
-
-      comments: dab.times (5, function (i, opts, callback) {
+      users: dab.times (2, function (i) { return {name: `User${i}`}; }),
+      comments: dab.times (5, function (i) {
         let user = dab.ref (dab.sample (dab.get ('users')));
-        let comment = {user: user, comment: 'This is comment ' + i};
-        return callback (null, comment);
+        return {user, comment: `This is comment ${i}`};
       })
     };
 
-    build (data, function (err, result) {
-      if (err)
-        return done (err);
-
+    return build (data).then (result => {
       // check the users
       expect (result.users).to.have.length (2);
 
       for (let i = 0, stop = result.users.length; i < stop; ++ i) {
-        expect (result).to.have.nested.property ('users[' + i + ']._id').that.is.instanceof (ObjectId);
-        expect (result).to.have.nested.property ('users[' + i + '].name', 'User' + i);
+        expect (result).to.have.nested.property (`users[${i}]._id`).that.is.instanceof (ObjectId);
+        expect (result).to.have.nested.property (`users[${i}].name`, `User${i}`);
       }
 
       // check the comments
       expect (result.comments).to.have.length (5);
 
-      async.each (result.comments, function (item, callback) {
-        expect (item).to.have.deep.property ('user').that.is.instanceof (ObjectId);
-
-        async.some (result.users, function (user, callback) {
-          return callback (null, user._id.equals (item.user));
-        }, callback);
-      }, done);
+      result.comments.forEach (comment => {
+        expect (comment).to.have.deep.property ('user').that.is.instanceof (ObjectId);
+        expect (some (result.users, user => user._id.equals (comment.user))).to.be.true;
+      });
     });
   });
 });
