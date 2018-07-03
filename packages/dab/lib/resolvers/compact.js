61a3ab4d785de77e622b45c9434980c91b0515f9
James H. Hill <hilljh82@gmail.com>
2018-07-03 15:43:36 -0400

    Added compact() resolver

--- packages/dab/lib/resolvers/compact.js
+++ packages/dab/lib/resolvers/compact.js
@@ -15,20 +15,15 @@
  *
  */
 
-exports.clone = require ('./clone');
-exports.computed = require ('./computed');
-exports.concat = require ('./concat');
-exports.filter = require ('./filter');
-exports.get = require ('./get');
-exports.id = require ('./id');
-exports.keys = require ('./keys');
-exports.map = require ('./map');
-exports.omit = require ('./omit');
-exports.ref = require ('./ref');
-exports.sample = require ('./sample');
-exports.shuffle = require ('./shuffle');
-exports.times = require ('./times');
-exports.compact = require ('./compact');
+const { compact } = require ('lodash');
 
-exports.string = require ('./string');
-exports.gridfs = require ('./gridfs');
+module.exports = function (array) {
+  return function __dabCompact () {
+    return this.resolve (array).then (array => {
+      if (array === undefined)
+        return undefined;
+
+      return compact (array);
+    });
+  };
+};
